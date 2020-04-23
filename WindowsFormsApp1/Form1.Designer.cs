47c8ec021439eeefd19f8c73e4ce58e87085a475
Sazid Nur Ratul <sazid35-346@diu.edu.bd>
2020-04-23 07:49:43 +0600

    initial

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboSheet = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.cboSheet2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.opnBtn2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboSheet
+            // 
+            this.cboSheet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSheet.FormattingEnabled = true;
+            this.cboSheet.Location = new System.Drawing.Point(268, 25);
+            this.cboSheet.Name = "cboSheet";
+            this.cboSheet.Size = new System.Drawing.Size(151, 21);
+            this.cboSheet.TabIndex = 0;
+            this.cboSheet.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(667, 574);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(227, 28);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(35, 13);
+            this.label.TabIndex = 2;
+            this.label.Text = "Sheet";
+            this.label.Click += new System.EventHandler(this.Sheet_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(322, 661);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(97, 29);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // cboSheet2
+            // 
+            this.cboSheet2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboSheet2.FormattingEnabled = true;
+            this.cboSheet2.Location = new System.Drawing.Point(982, 19);
+            this.cboSheet2.Name = "cboSheet2";
+            this.cboSheet2.Size = new System.Drawing.Size(121, 21);
+            this.cboSheet2.TabIndex = 4;
+            this.cboSheet2.SelectedIndexChanged += new System.EventHandler(this.cboSheet2_SelectedIndexChanged);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(685, 70);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(668, 574);
+            this.dataGridView2.TabIndex = 5;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // opnBtn2
+            // 
+            this.opnBtn2.Location = new System.Drawing.Point(1022, 661);
+            this.opnBtn2.Name = "opnBtn2";
+            this.opnBtn2.Size = new System.Drawing.Size(81, 29);
+            this.opnBtn2.TabIndex = 6;
+            this.opnBtn2.Text = "Open";
+            this.opnBtn2.UseVisualStyleBackColor = true;
+            this.opnBtn2.Click += new System.EventHandler(this.opnBtn2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(941, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Sheet";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.opnBtn2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.cboSheet2);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cboSheet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboSheet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ComboBox cboSheet2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button opnBtn2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Sheet_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        DataSet result, result2;
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+            cboSheet.Items.Clear();
+            
+            const string type = "Excel Workbook|*.xlsx; *.xls; *.xlt; *.xlm; *.xlsm; *.xltx; *.xltm; *.xlsb; *.xla; *.xlam; *.xll; *.xlw";
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = type, ValidateNames = true })
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    FileStream fs = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                    IExcelDataReader reader = ExcelReaderFactory.CreateReader(fs);
+                    result = reader.AsDataSet();
+                    fs.Close();
+                    foreach(DataTable dt in result.Tables)
+                    {
+                        cboSheet.Items.Add(dt.TableName);
+                    }
+                    cboSheet.SelectedIndex = 0;
+                    reader.Close();
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = result.Tables[cboSheet.SelectedIndex];
+        }
+
+        private void opnBtn2_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Columns.Clear();
+            cboSheet2.Items.Clear();
+            
+            const string type = "Excel Workbook|*.xlsx; *.xls; *.xlt; *.xlm; *.xlsm; *.xltx; *.xltm; *.xlsb; *.xla; *.xlam; *.xll; *.xlw";
+            using (OpenFileDialog ofd2 = new OpenFileDialog() { Filter = type, ValidateNames = true })
+            {
+                if (ofd2.ShowDialog() == DialogResult.OK)
+                {
+                    FileStream fs2 = File.Open(ofd2.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                    IExcelDataReader reader2 = ExcelReaderFactory.CreateReader(fs2);
+                    result2 = reader2.AsDataSet();
+                    fs2.Close();
+                    foreach (DataTable dt in result2.Tables)
+                    {
+                        cboSheet2.Items.Add(dt.TableName);
+                    }
+                    cboSheet2.SelectedIndex = 0;
+                    reader2.Close();
+                }
+            }
+        }
+
+        private void cboSheet2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView2.DataSource = result2.Tables[cboSheet2.SelectedIndex];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
